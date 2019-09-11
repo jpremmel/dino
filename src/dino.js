f079a19ac414d8c49a9269b42c55158205b9f783
Jacqueline Remmel and Lauren Musante <jacqueline@>
2019-09-11 14:05:04 -0700

    Add ability to query dinosaur for a random fact. Add notes for reference

--- src/dino.js
+++ src/dino.js
@@ -30,3 +30,21 @@ export class DinoMaker {
     });
   }
 }
+
+export class Conversation {
+  generateFact() { // Conversation only has 1 method and that's it. Not a property.
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://uselessfacts.jsph.pl/random.json?language=en`; // unique to this promise. unique API. Sometimes a key isn't needed; only the URL
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,31 +2,42 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { DinoMaker } from "./dino.js";
+import { DinoMaker, Conversation } from "./dino.js";
+
 
 $(document).ready(function() {
   $("#dino").submit(function(event) {
     event.preventDefault();
     $("#dino").hide();
-    let dinoMaker = new DinoMaker();
-    let namePromise = dinoMaker.getDinoName();
-    let gifPromise = dinoMaker.getDinoGif();
+    let dinoMaker = new DinoMaker(); //imported from dino.js. Has the method getDinoName() defined within it.
+    let name; //to create a variable where we will eventually store the dino's name for later use.
 
+    let namePromise = dinoMaker.getDinoName();
     namePromise.then(function(response) {
-      const dinoName = JSON.parse(response);
-      $('.output').append(`<h4>Your dino's name is ${dinoName[0][0]}</h4>`);
+      const dinoName = JSON.parse(response); // main nameless array in Postman
+      name = dinoName[0][0]; //access the main array at index 0 and the subarray at index 0. 00 since we are only grabbing one word from one paragraph
+      $('.output').append(`<h4>Your dino's name is ${name}!</h4>`);
+      $('#dino-name').text(name); // name was declared on line 13 and defined on line 18
     });
 
+    let gifPromise = dinoMaker.getDinoGif();
     gifPromise.then(function(response) {
       const gifResponse = JSON.parse(response);
-      $(".output").append(`<img src="${gifResponse.data.images.downsized_large.url}">`);
+      $(".output").append(`<img src="${gifResponse.data.images.downsized_medium.url}">`);
     });
-
-
-
     $(".output").show();
+    $(".dino-name").text(name);
+    $(".conversation").show();
 
+    let conversation = new Conversation();
+    $("#query").on("click", function() {
+      let fact = conversation.generateFact(); //running generateFact on conversation (new instance of Conversation, declared in line 32) generateFact returns a new instance of Promise object.
+      fact.then(function(response) { // .then only executes callback function after promise has been fulfilled.
+        const generatedFact = JSON.parse(response);
+        $('.conversation').append(`<p><strong>${name} says:</strong> "${generatedFact.text}"</p>`);
+      })
 
+    });
   });
 
 });
