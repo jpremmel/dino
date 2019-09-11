87c8cb108d411fdd1c36a0ce552025f25636004e
Jacqueline Remmel and Lauren Musante <lauren@>
2019-09-11 11:01:09 -0700

    Add dino name API call

---
+++ src/dino.js
@@ -0,0 +1,17 @@
+export class DinoMaker {
+  getDino() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=1`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,8 +2,28 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { DinoMaker } from "./dino.js";
 
 $(document).ready(function() {
+  $("#dino").submit(function(event) {
+    event.preventDefault();
+    $("#dino").hide();
+    let dinoMaker = new DinoMaker();
+    let promise = dinoMaker.getDino();
 
+    promise.then(function(response) {
+      const dinoName = JSON.parse(response);
+
+
+      $('.output').append(`<h4>Your dino's name is ${dinoName[0][0]}</h4>`);
+    });
+
+
+
+
+    $(".output").show();
+
+
+  });
 
 });
